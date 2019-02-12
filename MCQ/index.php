b45a37a34d290cf4ddcae744148113149c4ce066
Raghav Virmani <42843368+robovirmani@users.noreply.github.com>
2019-02-13 00:02:13 +0530

    Add files via upload

---
+++ MCQ/index.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<title>R.A.M</title>
+<link  rel="stylesheet" href="css/bootstrap.min.css"/>
+ <link  rel="stylesheet" href="css/bootstrap-theme.min.css"/>    
+ <link rel="stylesheet" href="css/main.css">
+ <link  rel="stylesheet" href="css/font.css">
+ <script src="js/jquery.js" type="text/javascript"></script>
+
+  <script src="js/bootstrap.min.js"  type="text/javascript"></script>
+ 	<link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
+<?php if(@$_GET['w'])
+{echo'<script>alert("'.@$_GET['w'].'");</script>';}
+?>
+<script>
+function validateForm() {var y = document.forms["form"]["name"].value;	var letters = /^[A-Za-z]+$/;if (y == null || y == "") {alert("Name must be filled out.");return false;}var z =document.forms["form"]["college"].value;if (z == null || z == "") {alert("college must be filled out.");return false;}var x = document.forms["form"]["email"].value;var atpos = x.indexOf("@");
+var dotpos = x.lastIndexOf(".");if (atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length) {alert("Not a valid e-mail address.");return false;}var a = document.forms["form"]["password"].value;if(a == null || a == ""){alert("Password must be filled out");return false;}if(a.length<5 || a.length>25){alert("Passwords must be 5 to 25 characters long.");return false;}
+var b = document.forms["form"]["cpassword"].value;if (a!=b){alert("Passwords must match.");return false;}}
+</script>
+
+
+</head>
+
+<body>
+<div class="header">
+<div class="row">
+
+<div class="col-lg-6" style="padding-top: 14px" >
+<span class="logo"><a href="../home.php"><img src="image/logo.png" height="40px" width="150px"></a></span></div>
+
+<div class="col-md-2 col-md-offset-4" style="padding-top: 20px;">
+<a data-toggle="modal" data-target="#myModal"><button class="btn btn-info">Login</button></a></div>
+<!--sign in modal start-->
+<div class="modal fade" id="myModal">
+  <div class="modal-dialog">
+    <div class="modal-content title1">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title title1"><span style="color:orange">Log In</span></h4>
+      </div>
+      <div class="modal-body">
+        <form class="form-horizontal" action="login.php?q=index.php" method="POST">
+<fieldset>
+
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-3 control-label" for="email"></label>  
+  <div class="col-md-6">
+  <input id="email" name="email" placeholder="Enter your email-id" class="form-control input-md" type="email">
+    
+  </div>
+</div>
+
+
+<!-- Password input-->
+<div class="form-group">
+  <label class="col-md-3 control-label" for="password"></label>
+  <div class="col-md-6">
+    <input id="password" name="password" placeholder="Enter your Password" class="form-control input-md" type="password">
+    
+  </div>
+</div>
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="submit" class="btn btn-primary">Log in</button>
+		</fieldset>
+</form>
+      </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+<!--sign in modal closed-->
+
+</div><!--header row closed-->
+</div>
+
+<div class="bg1">
+<div class="row">
+<br><br><br>
+<div class="col-md-7"></div>
+<div class="col-md-3 panel" style="margin-left: 500px; background-color: transparent;">
+<!-- sign in form begins -->  
+  <form class="form-horizontal" name="form" action="sign.php?q=account.php" onSubmit="return validateForm()" method="POST">
+<fieldset>
+
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-12 control-label" for="name"></label>  
+  <div class="col-md-12">
+  <input id="name" name="name" placeholder="Enter your name" class="form-control input-md" type="text">
+    
+  </div>
+</div>
+
+<!-- Text input-->
+
+
+<!-- Text input-->
+
+
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-12 control-label title1" for="email"></label>
+  <div class="col-md-12">
+    <input id="email" name="email" placeholder="Enter your email-id" class="form-control input-md" type="email">
+    
+  </div>
+</div>
+
+<!-- Text input-->
+
+
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-12 control-label" for="password"></label>
+  <div class="col-md-12">
+    <input id="password" name="password" placeholder="Enter your password" class="form-control input-md" type="password">
+    
+  </div>
+</div>
+
+<div class="form-group">
+  <label class="col-md-12control-label" for="cpassword"></label>
+  <div class="col-md-12">
+    <input id="cpassword" name="cpassword" placeholder="Confirm Password" class="form-control input-md" type="password">
+    
+  </div>
+</div>
+<?php if(@$_GET['q7'])
+{ echo'<p style="color:red;font-size:15px;">'.@$_GET['q7'];}?>
+<!-- Button -->
+<div class="form-group">
+  <label class="col-md-12 control-label" for=""></label>
+  <div class="col-md-12" style="margin-left: 100px"> 
+    <input  type="submit" value="Sign Up" class="btn btn-success btn-lg"/>
+  </div>
+</div>
+
+</fieldset>
+</form>
+</div><!--col-md-6 end-->
+</div></div>
+
+</div><!--container end-->
+
+<!--Footer start-->
+<div class="row footer">
+
+<div class="col-md-2 box" style="margin-left: 42%">
+<a href="#" data-toggle="modal" data-target="#login">Admin Login</a></div>
+
+
+
+
+<!--Modal for admin login-->
+   <div class="modal fade" id="login">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+        <h4 class="modal-title"><span style="color:orange;font-family:'typo' ">LOGIN</span></h4>
+      </div>
+      <div class="modal-body title1">
+<div class="row">
+<div class="col-md-3"></div>
+<div class="col-md-6">
+<form role="form" method="post" action="admin.php?q=index.php">
+<div class="form-group">
+<input type="text" name="uname" maxlength="20"  placeholder="Admin user id" class="form-control"/> 
+</div>
+<div class="form-group">
+<input type="password" name="password" maxlength="15" placeholder="Password" class="form-control"/>
+</div>
+<div class="form-group" align="center">
+<input type="submit" name="login" value="Login" class="btn btn-primary" />
+</div>
+</form>
+</div><div class="col-md-3"></div></div>
+      </div>
+      <!--<div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>-->
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+<!--footer end-->
+
+</body>
+</html>
